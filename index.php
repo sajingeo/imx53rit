526f42085bd67b0184d11bf85743947bf12222e5
Sajin <sajin.geo@gmail.com>
2012-10-12 03:59:49 -0400

    code working on my mac

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+</head>
+
+<body>
+
+	<div id="contents" style="height:580px;">
+
+	<div id="iframes" style="float:left;">
+	
+		<iframe id="slide_show_sites" src="slideshow.html" width="700" height="520"></iframe>	
+		</div>
+		
+		
+		<div id="ttweets">
+		<iframe id="slide_show_sites" src="twitter.html" width="300" height="520"></iframe>	
+		</div>
+		
+	</div>
+
+	<div id="fotter" align="center">
+			<span style="font-size: 43px; position: relative; top: -40px;">Powered By:</span>
+			<img src="fotter_logo.jpg" height="100px" />
+	</div>
+
+	</body>
+</html>
---
+++ twitter.com
@@ -0,0 +1,35 @@
+<html>
+<body>
+<script src="http://widgets.twimg.com/j/2/widget.js"></script>
+<script>
+new TWTR.Widget({
+  version: 2,
+  type: 'profile',
+  rpp: 4,
+  interval: 6000,
+  width: 'auto',
+  height: 300,
+  theme: {
+    shell: {
+      background: '#333333',
+      color: '#ffffff'
+    },
+    tweets: {
+      background: '#000000',
+      color: '#ffffff',
+      links: '#4aed05'
+    }
+  },
+  features: {
+    scrollbar: true,
+    loop: false,
+    live: true,
+    hashtags: true,
+    timestamp: true,
+    avatars: false,
+    behavior: 'all'
+  }
+}).render().setUser('mycrobonics').start();
+</script>
+</body>
+</html>
